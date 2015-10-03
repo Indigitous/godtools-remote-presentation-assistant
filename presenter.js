f3a4bd5d9acb7829d13e8d6a08d27649666600cd
Sheldon Dueck <sheldon.dueck@gmail.com>
2015-10-03 15:04:06 -0400

    This doesn't work

--- presenter.js
+++ presenter.js
@@ -1,1 +1,9 @@
-var fb = new Firebase('https://h6w4gfw5itf.firebaseio-demo.com/');
+var fb = new Firebase('https://blinding-fire-1510.firebaseio.com/');
+var test = fb.set({
+  "xyz":
+  {
+    password: "test",
+    html_content: "hello Sheldon",
+    knowgod_url: "http://knowgod.com/en/kgp/1/"
+  }
+});
--- viewer.js
+++ viewer.js
@@ -1,16 +1,28 @@
 var additional_css = "nav { display: none !important; }";
 
-$.get("http://knowgod.com/en/kgp/1").success(function(response) {
+var fb = new Firebase('https://blinding-fire-1510.firebaseio.com/');
 
-  // Fetch html
-  var knowgod_html = $(response).find('article').first();
-  $('#view').html(knowgod_html);
+var session = fb.child('xyz');
 
-  // Fetch css
-  var knowgod_css = response.match(/href=\"(\/css\/main([^\s]*.css))\"/)[1];
-  $('<link href="http://knowgod.com' + knowgod_css + '" rel="stylesheet">').prependTo('#view');
-
-  // Additional css
-  $('<style type="text/css">' + additional_css + '</style>').prependTo('#view');
-
+session.on('value', function(snapshot) {
+  var session_data = snapshot.val();
+  var knowgod_url = session_data.knowgod_url;
+  load_view_from_knowgod(knowgod_url);
 });
+
+function load_view_from_knowgod(url) {
+  $.get(url).success(function(response) {
+
+    // Fetch html
+    var knowgod_html = $(response).find('article').first();
+    $('#view').html(knowgod_html);
+
+    // Fetch css
+    var knowgod_css = response.match(/href=\"(\/css\/main([^\s]*.css))\"/)[1];
+    $('<link href="http://knowgod.com' + knowgod_css + '" rel="stylesheet">').prependTo('#view');
+
+    // Additional css
+    $('<style type="text/css">' + additional_css + '</style>').prependTo('#view');
+
+  });
+}
