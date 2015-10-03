09539f6c6142c112114901090c4a2d9a6c217c4b
Sheldon Dueck <sheldon.dueck@gmail.com>
2015-10-03 14:39:24 -0400

    Load knowgod css dynamically

--- viewer.js
+++ viewer.js
@@ -1,4 +1,11 @@
 $.get("http://knowgod.com/en/kgp/1").success(function(response) {
-  var view_content = $(response).find('article').first();
-  $('#view').html(view_content);
+
+  // Fetch html
+  var knowgod_html = $(response).find('article').first();
+  $('#view').html(knowgod_html);
+
+  // Fetch css
+  var knowgod_css = response.match(/href=\"(\/css\/main([^\s]*.css))\"/)[1];
+  $('<link href="http://knowgod.com' + knowgod_css + '" rel="stylesheet">').prependTo('#view');
+
 });
