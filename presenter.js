75d8778c9fb958fc5e426d508565e80c7ea7d9d2
Suraj Reddy <suraj@mozzaz.com>
2015-10-03 16:18:10 -0400

    adding dropdowns

--- presenter.js
+++ presenter.js
@@ -1,8 +1,11 @@
+var Presenter = {};
 var viewer_url = 'http://indigitous.github.io/godtools-remote-presentation-assistant/viewer.html'
 
 $(document).ready(function() {
   var session = generate_session_guid();
   var url = 'http://knowgod.com/en/fourlaws/1/';
+
+  Presenter.populate_dropdowns();
 
   $('#viewer_link').val(viewer_url + '?session=' + session);
 
--- suraj/presenter.js
+++ suraj/presenter.js
@@ -1,10 +1,9 @@
 var fb = new Firebase('https://blinding-fire-1510.firebaseio.com/');
-var test = fb.set({
-        "xyz": 
-        {
-            password: "test",
-            html_content: "hello Sheldon"
-        }
-    });
-var post_id = test.key();
-alert(post_id);
+// var test = fb.set({
+//         "xyz": 
+//         {
+//             password: "test",
+//             html_content: "hello Sheldon"
+//         }
+//     });
+$('#know_god')
